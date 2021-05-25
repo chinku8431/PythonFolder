bbda7c0d9d3892ff3f2adf738ed0f8c7daa8aed2
chinku8431 <asimkhandai@gmail.com>
2021-05-25 16:28:32 +0530

    length of string

---
+++ StringProgrammme/lengthstring.py
@@ -0,0 +1,14 @@
+
+
+namestring=input("Enter the name")
+print(len(namestring))
+
+print("--------------Method-----Two")
+
+name=input("Enter the string")
+count=0;
+for x in name:
+    count=count+1
+
+print("The value of count",count)
+
