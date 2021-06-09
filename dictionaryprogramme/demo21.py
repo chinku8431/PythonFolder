a9e8c88db315dc9836e8019dcd5acb7b4b414d61
chinku8431 <asimkhandai@gmail.com>
2021-06-09 16:45:51 +0530

    Adding OOPS docs

--- dictionaryprogramme/demo21.py
+++ dictionaryprogramme/demo21.py
@@ -8,7 +8,13 @@ def showindex():
     d=[[k,v] for x in emp_info for k,v in x.items()]
     print("The value of d ----->>",d)
     d1=[]
-    d1.append(d)
-    print("The value od d1",d1)
+    d2=[]
+    for d1 in d:
+        print(d1)
+        d2=d1+d2
+        print(d2)
+        
 
 showindex()
+
+
